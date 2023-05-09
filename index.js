92e429cf9822e87d43936a52e8228ccba160d8bf
AdanMC <dev.adanmc01@gmail.com>
2023-05-09 00:01:09 -0600

    addredis

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+ require('dotenv').config()
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
@@ -6,14 +7,19 @@ const app = express();
 let server = http.createServer(app);
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 8080;
+const redisAdapter = require('socket.io-redis');
+const redisUrl = process.env.REDIS_URL || 'redis://localhost:6379';
 
 app.use(express.static(publicPath));
 //IO = es la comunicacion del backend
 module.exports.io = socketIO(server,{ 
     cors: {
         origin: '*',
-    }});
+    },
+    presence: true,
+});
 require('./src/controllers/sockets.js')
+io.adapter(redisAdapter(redisUrl));
 
 server.listen(port, (err) => {
 
--- src/controllers/sockets.js
+++ src/controllers/sockets.js
@@ -1,30 +1,37 @@
-const {io} = require ('../../index')
+const { io } = require("../../index");
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
-    organization: "org-EGH0s1FOJiAtzwVKqvy2RSTw",
-    apiKey:'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',
+  organization: process.env.OPENAI_ORGANIZATION,
+  apiKey: process.env.OPENAI_APIKEY,
 });
 const openai = new OpenAIApi(configuration);
 
-
 io.on("connection", (socket) => {
+  try {
     console.log("A user connected.");
-  
     socket.on("message", async (message) => {
       console.log("Message received:", message);
       const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{role: "user", content: `${message}`}],
+        messages: [{ role: "user", content: `${message}` }],
       });
       console.log(completion.data.choices[0].message.content);
-      io.emit("message", {
-        username:completion.data.choices[0].message.role,
-        text: completion.data.choices[0].message.content,
-        time: Date.now(),
-      });
+      const resp = completion.data.choices[0].message.content;
+      if (resp) {
+        io.emit("message", {
+          username: completion.data.choices[0].message.role,
+          text: completion.data.choices[0].message.content,
+          time: Date.now(),
+        });
+      } else {
+        throw new Error("OpenAI response was empty");
+      }
     });
     socket.on("disconnect", () => {
       console.log("A user disconnected.");
     });
-  });
-
+  } catch (error) {
+    console.error("Error with OpenAI API:", error);
+    io.emit("error", "Error with OpenAI API");
+  }
+});
